c8ea393703cca60e431dea5d03c1ac54994803a1
Monty Brandenberg <monty@lindenlab.com>
2014-05-19 17:51:31 +0000

    Mac/Linux:  rehabilitate repo structure, update to 8.35.  Build with intended options (Unicode support).  Package and include documentation.

--- build-cmd.sh
+++ build-cmd.sh
@@ -27,9 +27,9 @@ stage="${top}"/stage
 case "$AUTOBUILD_PLATFORM" in
 
     "windows")
-	    echo "Windows not ready for builds yet." 1>&2
-		fail
-	;;
+        echo "Windows not ready for builds yet." 1>&2
+        fail
+    ;;
 
     "darwin")
         pushd pcre
@@ -46,12 +46,19 @@ case "$AUTOBUILD_PLATFORM" in
             
             opts="${TARGET_OPTS:--arch i386 -iwithsysroot $sdk -mmacosx-version-min=10.6}"
 
-			# Debug first
+            # Prefer llvm-g++ if available.
+            if [ -x /usr/bin/llvm-gcc -a -x /usr/bin/llvm-g++ ]; then
+                export CC=/usr/bin/llvm-gcc
+                export CXX=/usr/bin/llvm-g++
+            fi
+
+            # Debug first
             CFLAGS="$opts -O0 -gdwarf-2" CXXFLAGS="$opts -O0 -gdwarf-2" LDFLAGS="$opts -gdwarf-2" \
-				./configure --disable-dependency-tracking --with-pic \
-				--prefix="$stage" --includedir="$stage"/include/pcre --libdir="$libdir"/debug
+                ./configure --disable-dependency-tracking --with-pic --enable-utf --enable-unicode-properties \
+                --enable-static=yes --enable-shared=no \
+                --prefix="$stage" --includedir="$stage"/include/pcre --libdir="$libdir"/debug
             make 
-			make install
+            make install
 
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
@@ -60,12 +67,13 @@ case "$AUTOBUILD_PLATFORM" in
 
             make clean
 
-			# Release last for configuration headers
+            # Release last for configuration headers
             CFLAGS="$opts -O2 -gdwarf-2" CXXFLAGS="$opts -O2 -gdwarf-2" LDFLAGS="$opts -gdwarf-2" \
-				./configure --disable-dependency-tracking --with-pic \
-				--prefix="$stage" --includedir="$stage"/include/pcre --libdir="$libdir"/release
+                ./configure --disable-dependency-tracking --with-pic --enable-utf --enable-unicode-properties \
+                --enable-static=yes --enable-shared=no \
+                --prefix="$stage" --includedir="$stage"/include/pcre --libdir="$libdir"/release
             make 
-			make install
+            make install
 
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
@@ -114,12 +122,13 @@ case "$AUTOBUILD_PLATFORM" in
                 export CPPFLAGS="$TARGET_CPPFLAGS"
             fi
 
-			# Debug first
+            # Debug first
             CFLAGS="$opts -g -O0" CXXFLAGS="$opts -g -O0" LDFLAGS="$opts -g" \
-				./configure --with-pic \
-				--prefix="$stage" --includedir="$stage"/include/pcre --libdir="$libdir"/debug
+                ./configure --with-pic --enable-utf --enable-unicode-properties \
+                --enable-static=yes --enable-shared=no \
+                --prefix="$stage" --includedir="$stage"/include/pcre --libdir="$libdir"/debug
             make 
-			make install
+            make install
 
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
@@ -128,12 +137,13 @@ case "$AUTOBUILD_PLATFORM" in
 
             make clean
 
-			# Release last for header files
+            # Release last for header files
             CFLAGS="$opts -O2" CXXFLAGS="$opts -O2" LDFLAGS="$opts" \
-				./configure --with-pic \
-				--prefix="$stage" --includedir="$stage"/include/pcre --libdir="$libdir"/release
+                ./configure --with-pic --enable-utf --enable-unicode-properties \
+                --enable-static=yes --enable-shared=no \
+                --prefix="$stage" --includedir="$stage"/include/pcre --libdir="$libdir"/release
             make 
-			make install
+            make install
 
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
@@ -145,13 +155,15 @@ case "$AUTOBUILD_PLATFORM" in
     ;;
 
     *)
-	    echo "Unrecognized platform" 1>&2
-		fail
+        echo "Unrecognized platform" 1>&2
+        fail
     ;;
 esac
 
 mkdir -p stage/LICENSES
 cp "pcre/LICENCE" "stage/LICENSES/pcre-license.txt"
+mkdir -p "$stage"/docs/pcre/
+cp -a "$top"/README.Linden "$stage"/docs/pcre/
 
 pass
 
